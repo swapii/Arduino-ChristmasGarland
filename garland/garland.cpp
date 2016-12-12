6df65ac5ec4273dbb2b9b6b2b7540640c5d131eb
Pavel Savinov <swapii@gmail.com>
2016-12-12 23:45:24 +0300

    Pulse 1 LED
    GRL-2

--- garland/garland.cpp
+++ garland/garland.cpp
@@ -1,12 +1,70 @@
 #include <Arduino.h>
 
+#include "FastLED.h"
+
+#define NUM_LEDS 1
+CRGB leds[NUM_LEDS];
+
 void setup(void) {
-  pinMode(13, OUTPUT);
+    FastLED.addLeds<NEOPIXEL, 10>(leds, NUM_LEDS);
 }
 
 void loop(void) {
-  digitalWrite(13, HIGH);
-  delay(3000);
-  digitalWrite(13, LOW);
-  delay(3000);
+
+    leds[0] = CRGB::Red;
+    FastLED.show();
+    delay(1000);
+
+    leds[0] = CRGB::Black;
+    FastLED.show();
+    delay(300);
+
+    leds[0] = CRGB::Green;
+    FastLED.show();
+    delay(1000);
+
+    leds[0] = CRGB::Black;
+    FastLED.show();
+    delay(300);
+
+    leds[0] = CRGB::Blue;
+    FastLED.show();
+    delay(1000);
+
+    leds[0] = CRGB::Black;
+    FastLED.show();
+    delay(300);
+
+    leds[0] = CRGB::Yellow;
+    FastLED.show();
+    delay(1000);
+
+    leds[0] = CRGB::Black;
+    FastLED.show();
+    delay(300);
+
+    leds[0] = CRGB::Cyan;
+    FastLED.show();
+    delay(1000);
+
+    leds[0] = CRGB::Black;
+    FastLED.show();
+    delay(300);
+
+    leds[0] = CRGB::Gold;
+    FastLED.show();
+    delay(1000);
+
+    leds[0] = CRGB::Black;
+    FastLED.show();
+    delay(300);
+
+    leds[0] = CRGB::Purple;
+    FastLED.show();
+    delay(1000);
+
+    leds[0] = CRGB::Black;
+    FastLED.show();
+    delay(300);
+
 }
