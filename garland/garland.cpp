68ae590538046e58cf3e326795de8eb4688dacbf
Pavel Savinov <swapii@gmail.com>
2016-12-13 00:46:30 +0300

    Random color star
    GRL-3

--- garland/garland.cpp
+++ garland/garland.cpp
@@ -3,18 +3,41 @@
 #include "FastLED.h"
 
 #define NUM_LEDS 1
+
+struct Star {
+    CHSV color = CHSV(0, 255, 0);
+    bool growing = true;
+};
+
+Star stars[NUM_LEDS];
 CRGB leds[NUM_LEDS];
 
 void setup(void) {
+    stars[0] = Star();
     FastLED.addLeds<NEOPIXEL, 10>(leds, NUM_LEDS);
 }
 
 void loop(void) {
 
-    for (int i = 0; i < 255; i++) {
-        leds[0] = CHSV(i, 255, 128);
-        FastLED.show();
-        delay(10);
+    int i = 0;
+
+    Star &star = stars[i];
+    CHSV &color = star.color;
+
+    color.value += star.growing ? 3 : -1;
+
+    if (color.value > 100) {
+        star.growing = false;
     }
+
+    if (color.value == 0) {
+        star.growing = true;
+        color.hue = random8(255);
+    }
+
+    leds[i] = color;
+
+    FastLED.show();
+    delay(30);
 
 }
