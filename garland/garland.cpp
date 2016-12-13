c49ec612ced6db14759bd4b4fd2a40c9f039e2d7
Pavel Savinov <swapii@gmail.com>
2016-12-14 01:45:03 +0300

    5 LEDs with random star position
    GRL-3

--- garland/garland.cpp
+++ garland/garland.cpp
@@ -2,7 +2,8 @@
 
 #include "FastLED.h"
 
-#define NUM_LEDS 1
+#define NUM_LEDS 5
+#define MAX_VALUE 200
 
 struct Star {
     CHSV color = CHSV(0, 255, 0);
@@ -13,31 +14,39 @@ Star stars[NUM_LEDS];
 CRGB leds[NUM_LEDS];
 
 void setup(void) {
-    stars[0] = Star();
+
+    for (int i = 0; i < NUM_LEDS; i++) {
+        stars[i].color.hue = random8();
+        stars[i].color.value = random8(MAX_VALUE);
+        stars[i].growing = random8() > 128;
+    }
+
     FastLED.addLeds<NEOPIXEL, 10>(leds, NUM_LEDS);
 }
 
 void loop(void) {
 
-    int i = 0;
+    for (int i = 0; i < NUM_LEDS; i++) {
 
-    Star &star = stars[i];
-    CHSV &color = star.color;
+        Star &star = stars[i];
+        CHSV &color = star.color;
 
-    color.value += star.growing ? 3 : -1;
+        color.value += star.growing ? 3 : -1;
 
-    if (color.value > 100) {
-        star.growing = false;
+        if (color.value > MAX_VALUE) {
+            star.growing = false;
+        }
+
+        if (color.value == 0) {
+            star.growing = true;
+            color.hue = random8();
+        }
+
+        leds[i] = color;
+
     }
-
-    if (color.value == 0) {
-        star.growing = true;
-        color.hue = random8(255);
-    }
-
-    leds[i] = color;
 
     FastLED.show();
-    delay(30);
+    delay(20);
 
 }
